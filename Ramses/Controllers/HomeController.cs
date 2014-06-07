e4206ffd86165e11e1f8edc4f4315d8e6e2dbc9d
jacksonpradolima <pradolima@live.com>
2014-06-07 11:28:47 -0300

    Inserido as dependências do MVC e criado a GridMVC

---
+++ Ramses/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Ramses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ramses.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private readonly List<Client> clients = new List<Client>()
+        { 
+            new Client { Id = 1, Name = "Julio Avellaneda", Email = "julito_gtu@hotmail.com" },
+            new Client { Id = 2, Name = "Juan Torres", Email = "jtorres@hotmail.com" },
+            new Client { Id = 3, Name = "Oscar Camacho", Email = "oscar@hotmail.com" },
+            new Client { Id = 4, Name = "Gina Urrego", Email = "ginna@hotmail.com" },
+            new Client { Id = 5, Name = "Nathalia Ramirez", Email = "natha@hotmail.com" },
+            new Client { Id = 6, Name = "Raul Rodriguez", Email = "rodriguez.raul@hotmail.com" },
+            new Client { Id = 7, Name = "Johana Espitia", Email = "johana_espitia@hotmail.com" }
+        };
+
+        /// <summary>
+        /// Renderiza uma grid
+        /// </summary>
+        /// <returns>Pacial contendo a codificação da grid</returns>
+        public ActionResult GriClients()
+        {
+            return PartialView("_GridClients", clients);
+        }
+	}
+}
--- Ramses/Global.asax.cs
+++ Ramses/Global.asax.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
 
 namespace Ramses
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
