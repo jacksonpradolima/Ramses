8b1fbc1f8d3f9ebe319288da7022229d8e10e3d1
jacksonpradolima <pradolima@live.com>
2014-06-11 16:53:32 -0300

    Criado o Biz de CotaParlamentar

---
+++ Ramses.Business/CotaParlamentarBiz.cs
@@ -0,0 +1,18 @@
+﻿using Ramses.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Ramses.Business
+{
+    public class CotaParlamentarBiz : BaseBusiness<CotaParlamentar>
+    {
+        protected override string OnGetErrorMessage(ErrorType error, Exception ex)
+        {
+            throw new NotImplementedException();
+        }      
+    }
+}
