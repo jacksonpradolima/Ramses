b1a3381d78924ee8b45817493613475053883438
felipe436 <felipe436@gmail.com>
2014-06-13 22:12:25 -0300

    Controller de exemplo chart

---
+++ Ramses/Controllers/ChartExampleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ramses.Controllers
+{
+    public class ChartExampleController : Controller
+    {
+        //
+        // GET: /Chart/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
