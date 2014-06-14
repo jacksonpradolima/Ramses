6304cfaf59fe980f4a78cc692c99cb5fc8089025
felipe436 <felipe436@gmail.com>
2014-06-13 22:11:28 -0300

    JS do Highcharts no Layout e criacao de model do exemplo

---
+++ Ramses/Models/HcExample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ramses.Models
+{
+    public class HcExample
+    {
+        public string Monthname { get; set; }
+        public int count { get; set; }
+    }
+}
